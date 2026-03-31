a5e512e14d296dff1120d9826ea5d054ac8c6a75
MikhailSklian <misasklankov255@gmail.com>
2026-03-31 23:53:11 +0300

    Fix base path for GitHub Pages

--- public/sw.js
+++ public/sw.js
@@ -1,50 +1,8 @@
-// Service Worker для оффлайн работы
 const CACHE_NAME = 'blacksmith-notes-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.webmanifest',
-  '/favicon.svg'
+  '/blacksmith-notes/',
+  '/blacksmith-notes/index.html',
+  '/blacksmith-notes/manifest.webmanifest',
+  '/blacksmith-notes/favicon.svg'
 ];
-
-// Устанавливаем Service Worker и кэшируем файлы
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Кэш открыт');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Перехватываем запросы и отдаем из кэша
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Если файл в кэше - возвращаем его
-        if (response) {
-          return response;
-        }
-        // Иначе загружаем из сети
-        return fetch(event.request);
-      })
-  );
-});
-
-// Обновляем Service Worker
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// остальной код без изменений
