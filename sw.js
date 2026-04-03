c50c42a442314f3ab32ec4db0682358c5ad36576
misha <misasklankov@gmail.com>
2026-04-03 16:48:00 +0300

    Fresh build

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'blacksmith-notes-v1';
+const urlsToCache = [
+  '/blacksmith-notes/',
+  '/blacksmith-notes/index.html',
+  '/blacksmith-notes/manifest.webmanifest',
+  '/blacksmith-notes/favicon.svg'
+];
+// остальной код без изменений
